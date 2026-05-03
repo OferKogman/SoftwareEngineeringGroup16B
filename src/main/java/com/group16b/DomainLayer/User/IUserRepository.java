da5195e2d19b9c769be9eb80817e400cfa17a882
Dor Okabi <dorok@post.bgu.ac.il>
2026-05-03 14:49:21 +0300

    implemented user repository and added methods to user

--- src/main/java/com/group16b/DomainLayer/User/IUserRepository.java
+++ src/main/java/com/group16b/DomainLayer/User/IUserRepository.java
@@ -1,5 +1,40 @@
 package com.group16b.DomainLayer.User;
 
-public class IUserRepository {
+import java.util.HashMap;
+import java.util.Map;
 
+public class IUserRepository{
+    
+    private static IUserRepository instance;
+    private Map<Integer, User> users = new HashMap<>();
+    
+    private IUserRepository() {
+    }
+    
+    public static synchronized IUserRepository getInstance() {
+        if (instance == null) {
+            instance = new IUserRepository();
+        }
+        return instance;
+    }
+    
+    public User getUserByID(int userID) {
+        return users.get(userID);
+    }
+    
+    public void addUser(User user) {
+        users.put(user.getUserID(), user);
+    }
+    
+    public void updateUser(User user) {
+        users.put(user.getUserID(), user);
+    }
+    
+    public void deleteUser(int userID) {
+        users.remove(userID);
+    }
+    
+    public boolean userExists(int userID) {
+        return users.containsKey(userID);
+    }
 }
--- src/main/java/com/group16b/DomainLayer/User/User.java
+++ src/main/java/com/group16b/DomainLayer/User/User.java
@@ -5,6 +5,7 @@ import com.group16b.DomainLayer.User.Roles.Role;
 
 public class User {
     
+    private int userID; 
     private String email;
     private String password;
     private HashMap<Integer, Role> roles; // Key: companyID, Value: Role
@@ -22,5 +23,19 @@ public class User {
     }
     public Role getRole(int companyID) {
         return roles.get(companyID);
+    }
+    public void addRole(int companyID, Role role) {
+        roles.put(companyID, role);
+    }
+    public User getParentForCompany(int companyID) {
+        Role role = roles.get(companyID);
+        if (role != null) {
+            int parentID = role.getParentID();
+            return IUserRepository.getInstance().getUserByID(parentID);
+        }
+        return null; // No role for this company, hence no parent
+    }
+    public int getUserID() {
+        return userID;
     }
 }
