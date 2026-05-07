53dd1ce2477e296d89a1c85c317599efc046bcc3
david podvinsky <davidpod@post.bgu.ac.il>
2026-05-07 14:44:13 +0300

    refined the interface, fixed eventService getting user from authService, instead it only gets the id and gets the user itself

--- src/main/java/com/group16b/ApplicationLayer/Interfaces/IAuthenticationService.java
+++ src/main/java/com/group16b/ApplicationLayer/Interfaces/IAuthenticationService.java
@@ -6,12 +6,12 @@ will allow treating admin and user seperetly for security reasons
 will issue the tokens and validate them
 will also act as a parser to extract info from the tokens when needed
 */
-public interface IAuthService {
-        String authenticateUser(String username, String password);
+public interface IAuthenticationService {
+        String authenticate(String username, String password);
         String authenticateAdmin(String username, String password);
         boolean validateUserToken(String token);
         boolean validateAdminToken(String token);
 
-        String extractIdFromUserToken(String token);
-        String extractIdFromAdminToken(String token);
+        int extractIdFromUserToken(String token);
+        int extractIdFromAdminToken(String token);
 }
