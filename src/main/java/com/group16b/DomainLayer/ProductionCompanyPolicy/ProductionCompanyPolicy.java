f95ae2e47048c477b8239c1c7c73f1ccc89e1baf
unknown <idofraiberg420@gmail.com>
2026-05-09 14:20:49 +0300

    Ticket #5 finished ProductionCompanyPolicy skeleton, pending review before pushing to main
    # Conflicts:
    #	src/main/java/com/group16b/DomainLayer/ProductionCompanyPolicy/ProductionCompanyPolicy.java

--- src/main/java/com/group16b/DomainLayer/ProductionCompanyPolicy/ProductionCompanyPolicy.java
+++ src/main/java/com/group16b/DomainLayer/ProductionCompanyPolicy/ProductionCompanyPolicy.java
@@ -1,23 +1,62 @@
 package com.group16b.DomainLayer.ProductionCompanyPolicy;
 
-import java.util.List;
-
-import com.group16b.DomainLayer.User.User;
+import com.group16b.DomainLayer.Policies.DiscountPolicy;
+import com.group16b.DomainLayer.Policies.PurchasePolicy;
 
 public class ProductionCompanyPolicy {
-    private int productionCompanyID;
-    private double rating;
-    
-    public int getProductionCompanyID() {
-        return productionCompanyID;
+    private int id;
+    private String name;
+    private CompanyState companyState;
+    private PurchasePolicy purchasePolicy;
+    private DiscountPolicy discountPolicy;
+
+    ProductionCompanyPolicy(int id, String name, CompanyState companyState, PurchasePolicy purchasePolicy,
+            DiscountPolicy discountPolicy) {
+        this.id = id;
+        this.name = name;
+        this.companyState = companyState;
+        this.purchasePolicy = purchasePolicy;
+        this.discountPolicy = discountPolicy;
     }
 
-    public double getRating() {
-        return rating;
+    public int getId() {
+        return this.id;
     }
 
-    public List<User> getAssociatedUsers() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAssociatedUsers'");
+    public String getName() {
+        return this.name;
     }
+
+    public CompanyState getCompanyState() {
+        return this.companyState;
+    }
+
+    public PurchasePolicy getPurchasePolicy() {
+        return this.purchasePolicy;
+    }
+
+    public DiscountPolicy getDiscountPolicy() {
+        return this.discountPolicy;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompanyState(CompanyState companyState) {
+        this.companyState = companyState;
+    }
+
+    public void setPurchasePolicy(PurchasePolicy purchasePolicy) {
+        this.purchasePolicy = purchasePolicy;
+    }
+
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
 }
