752605efe48c9ef0de9d4b3bdaf3b36b541f6691
david podvinsky <davidpod@post.bgu.ac.il>
2026-05-09 19:46:44 +0300

    refined the tests for user aggragate, now checking that you can only accept invites when appropriate, now need to esnure you can only add invites when appropriate

--- src/test/java/com/group16b/DomainLayer/User/UserTests.java
+++ src/test/java/com/group16b/DomainLayer/User/UserTests.java
@@ -37,6 +37,24 @@ public class UserTests {
         assertThrows(RuntimeException.class, () -> user.acceptInvite(0, 0));
         assertDoesNotThrow(() -> user.acceptInvite(1, 1));
     }
+    @Test
+    void testPromotionInviteAcceptance()
+    {
+        User user = new User( "testuser", "hashedpassword");
+        assertThrows(RuntimeException.class, () -> user.acceptInvite(0, 0));
+        assertNull(user.getRole(0));
+        user.addInvite(0, 0, new Manager(0, EnumSet.allOf(ManagerPermissions.class)));
+        user.addInvite(0, 1, new Manager(0, EnumSet.allOf(ManagerPermissions.class)));
+        user.addInvite(0, 2, new Owner(0));
+        user.addInvite(0, 3, new Manager(0, EnumSet.allOf(ManagerPermissions.class)));
+        assertDoesNotThrow(() -> user.acceptInvite(0, 0));
+        assertEquals(Manager.class, user.getRole(0).getClass());
+        assertThrows(RuntimeException.class, () -> user.acceptInvite(0, 0));
+        assertThrows(RuntimeException.class, () -> user.acceptInvite(0, 1));
+        assertDoesNotThrow(() -> user.acceptInvite(0, 2));
+        assertThrows(RuntimeException.class, () -> user.acceptInvite(0, 3));
+    }
+
 
     @Test
     void testCheckingForUserRolePermissionsOwnerSuccess()
