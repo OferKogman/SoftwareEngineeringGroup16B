39cb7b74f20a8dc22b315bb0219616409e921d62
Piff13 <hartmaa@post.bgu.ac.il>
2026-05-09 14:09:35 +0300

    virtualqueue root, repository and tests

---
+++ src/main/java/com/group16b/DomainLayer/VirtualQueue/VirtualQueueRepository.java
@@ -0,0 +1,52 @@
+package com.group16b.DomainLayer.VirtualQueue;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class VirtualQueueRepository implements IVirtualQueueRepository{
+	private final static VirtualQueueRepository instance = new VirtualQueueRepository();
+	private final ConcurrentHashMap<Long, VirtualQueue> queues = new ConcurrentHashMap<>();
+
+	private VirtualQueueRepository() {
+	}
+
+	public static VirtualQueueRepository getInstance() {
+		return instance;
+	}
+
+	@Override
+	public VirtualQueue findVirtualQueueById(long id) {
+		VirtualQueue dbVirtualQueue = queues.get(id);
+		if (dbVirtualQueue == null) {
+			throw new IllegalArgumentException("Queue does not exist");
+		}
+
+		return dbVirtualQueue;
+	}
+
+
+	@Override
+	public synchronized void saveVirtualQueue(VirtualQueue virtualQueue) {
+		VirtualQueue currQueue = findVirtualQueueById(virtualQueue.getId());
+		VirtualQueue updatedQueue = new VirtualQueue(virtualQueue);
+		updatedQueue.setVersion(virtualQueue.getVersion() + 1);
+
+		boolean isReplaced = queues.replace(virtualQueue.getId(), currQueue, updatedQueue);
+		if (!isReplaced) {
+			throw new IllegalArgumentException(
+					"Virtual Queue " + virtualQueue.getId()
+							+ " version mismatch or concurrent modification. Expected verion " +
+							virtualQueue.getVersion() + ", but found " + currQueue.getVersion() + " at read time");
+		}
+	}
+
+	@Override
+    public synchronized void addVirtualQueue(VirtualQueue virtualQueue){
+		VirtualQueue currQueue = queues.get(virtualQueue.getId());
+		if (currQueue != null) {
+			throw new IllegalArgumentException("Virtual queue existing!");
+		}
+
+		queues.put(virtualQueue.getId(), virtualQueue);
+    }
+}
