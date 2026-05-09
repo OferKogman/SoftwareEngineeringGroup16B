c7b13adb60820309cf43d08e479cb1a4c3d327db
david podvinsky <davidpod@post.bgu.ac.il>
2026-05-09 14:56:50 +0300

    added permissions managment to manager, also refined clarity regarding parent, and changed it to Integer so founder can have no assigner

--- src/main/java/com/group16b/DomainLayer/User/Roles/Manager.java
+++ src/main/java/com/group16b/DomainLayer/User/Roles/Manager.java
@@ -1,13 +1,28 @@
 package com.group16b.DomainLayer.User.Roles;
 
+import java.util.Set;
+
 public class Manager extends Member {
-	private int parentID;
+	private Integer assignerID;
+	private Set<ManagerPermissions> permissions;
 
-	protected Manager(int parentID) {
-		this.parentID = parentID;
+
+	protected Manager(Integer assignerID, Set<ManagerPermissions> permissions) {
+		this.assignerID = assignerID;
+		this.permissions = permissions;
 	}
 
-	public int getParentID() {
-		return parentID;
+	public Integer getAssignerID() {
+		return assignerID;
 	}
+
+	public boolean hasPermission(ManagerPermissions permission) {
+		return permissions.contains(permission);
+	}
+
+	public Set<ManagerPermissions> getPermissions() {
+		return Set.copyOf(permissions);
+	}
+
+
 }
