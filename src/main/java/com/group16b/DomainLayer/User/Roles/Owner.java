0ac5d669e76f0755c9d8d1e40933a14c6a0559e9
david podvinsky <davidpod@post.bgu.ac.il>
2026-05-10 15:47:18 +0300

    concetrated roleType in Manager to allow it to be final, since on promotion/demotion we fully change the role anyway

--- src/main/java/com/group16b/DomainLayer/User/Roles/Owner.java
+++ src/main/java/com/group16b/DomainLayer/User/Roles/Owner.java
@@ -8,29 +8,55 @@ public class Owner extends Manager {
 	private final List<Manager> assignedManagers;
 
 	public Owner(Integer parentID) {
-		super(parentID,EnumSet.allOf(ManagerPermissions.class));
-		this.roleType = RoleType.OWNER;
+		super(parentID,EnumSet.allOf(ManagerPermissions.class), RoleType.OWNER);
 		assignedManagers= new ArrayList<>();
 	}
 
+	protected Owner(Integer parentID, RoleType role)
+	{
+		super(parentID, EnumSet.allOf(ManagerPermissions.class),role);
+		assignedManagers= new ArrayList<>();
+	}
+
+
 	public List<Manager> getAssignedManagers() {
-		return new ArrayList<>(assignedManagers);//we dont want to return the original list to prevent external modification
+		ArrayList<Manager> copyList;
+		synchronized(lock)
+		{
+			copyList=new ArrayList<>(assignedManagers);
+		}
+		return copyList;//we dont want to return the original list to prevent external modification
 	}
 
 	//assign a manager to this owner
 	public void assignManager(Manager manager) {
-		assignedManagers.add(manager);
+		synchronized(lock)
+		{
+			assignedManagers.add(manager);
+		}
 	}
 
+	//ahu sharmuta needs lock
 	//remove a manager from this owner, if the manager is an owner, move all of its assigned managers to this owner before removing it
 	//if the manager isnt a direct asignee of this owner
 	public void removeManager(Manager manager) {
-		assignedManagers.remove(manager);
-		if(manager instanceof Owner){
-			//if the manager is an owner, move all of its assigned managers to this owner before removing it
-			Owner owner = (Owner) manager;
-			assignedManagers.addAll(owner.getAssignedManagers());
-			owner.getAssignedManagers().clear();
+		synchronized(lock)
+		{	
+			if(!assignedManagers.contains(manager))
+				return;
+
+			assignedManagers.remove(manager);
+			if(manager instanceof Owner){
+				//if the manager is an owner, move all of its assigned managers to this owner before removing it
+				Owner owner = (Owner) manager;
+				int thisID=owner.getAssignerID();
+				List<Manager> orphans=owner.getAssignedManagers();
+				for(Manager orphan:orphans)
+				{
+					orphan.setParent(thisID);
+					assignedManagers.add(orphan);
+				}
+			}
 		}
 	}
 
