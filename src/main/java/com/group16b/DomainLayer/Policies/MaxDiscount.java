50410bcfb350459e185be50a21865187db4e796e
unknown <idofraiberg420@gmail.com>
2026-05-20 00:53:27 +0300

    Added composites to discount, Max and Sum

---
+++ src/main/java/com/group16b/DomainLayer/Policies/MaxDiscount.java
@@ -0,0 +1,19 @@
+package com.group16b.DomainLayer.Policies;
+
+import java.util.List;
+
+public class MaxDiscount implements DiscountPolicy {
+    private List<DiscountPolicy> policies;
+
+    public MaxDiscount(List<DiscountPolicy> policies) {
+        if (policies == null || policies.isEmpty()) {
+            throw new IllegalArgumentException("MaxDiscount must have at least one policy.");
+        }
+        this.policies = policies;
+    }
+
+    public List<DiscountPolicy> getPolicies() { return policies; }
+
+    @Override
+    public double calculateDiscount(double originalPrice) { return originalPrice; }
+}
