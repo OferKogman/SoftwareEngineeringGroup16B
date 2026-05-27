6082aac02a5ed2aa3250355fe32b55cc273d33db
NicoleGluhovsky <nicolegl@post.bgu.ac.il>
2026-05-27 13:40:43 +0300

    it didnt break !

--- frontend/src/Components/ViewPurchasePolicies.tsx
+++ frontend/src/Components/ViewPurchasePolicies.tsx
@@ -1,55 +1,55 @@
-
 import type { PurchasePolicyDTO } from "../DTOs/PurchasePolicyDTO";
 
-type ViewPurchasePoliciesProps = {
+type PurchasePolicyProps = {
   purchasePolicy: PurchasePolicyDTO | null;
 };
 
-export default function ViewPurchasePolicies(
-    { purchasePolicy }: ViewPurchasePoliciesProps ) {
-        return (
-            <div>
-                {purchasePolicy ? (
-                    <div>
-                        {purchasePolicy.type === "Composite" && (
-                            <div>
-                                {purchasePolicy.leftPolicy && (
-                                    <div>
-                                        <ViewPurchasePolicies purchasePolicy={purchasePolicy.leftPolicy} />
-                                    </div>
-                                )}
-                                <p>{purchasePolicy.operator}</p>
-                                {purchasePolicy.rightPolicy && (
-                                    <div>
-                                        <ViewPurchasePolicies purchasePolicy={purchasePolicy.rightPolicy} />
-                                    </div>
-                                )}
-                            </div>
-                        )}
-                        {purchasePolicy.type === "Minimum Age" && (
-                            <p>Minimum Age: {purchasePolicy.minAge}</p>
-                        )}
-                        {purchasePolicy.type === "Maximum Age" && (
-                            <p>Maximum Age: {purchasePolicy.maxAge}</p>
-                        )}
-                        {purchasePolicy.type === "Minimum Tickets Per Customer" && (
-                            <p>Minimum Tickets: {purchasePolicy.minTickets}</p>
-                        )}
-                        {purchasePolicy.type === "Maximum Tickets Per Customer" && (
-                            <p>Maximum Tickets: {purchasePolicy.maxTickets}</p>
-                        )}
-                        {purchasePolicy.type === "Lottery" && (
-                            <div>
-                                <p>Lottery Name: {purchasePolicy.lotteryName}</p>
-                                <p>Winner Count: {purchasePolicy.lotteryWinnerCount}</p>
-                                <p>Registration Due Date: {purchasePolicy.lotteryRegistrationDueDate}</p>
-                            </div>
-                        )}
-                    </div>
-                ) : (
-                    <p>No purchase policy available.</p>
-                )}
-            </div>
-        )
-
+export default function ViewPurchasePolicies({ purchasePolicy }: PurchasePolicyProps) {
+    if (!purchasePolicy) { 
+        return <span>No purchase policy.</span>;
     }
+  // LEAF
+  if (purchasePolicy && purchasePolicy.type !== "Composite") {
+    return (
+      <span>
+        {purchasePolicy.type === "Minimum Age" && `Minimum Age: ${purchasePolicy.minAge}`}
+        {purchasePolicy.type === "Maximum Age" && `Maximum Age: ${purchasePolicy.maxAge}`}
+        {purchasePolicy.type === "Minimum Tickets Per Customer" &&
+          `Minimum Tickets: ${purchasePolicy.minTickets}`}
+        {purchasePolicy.type === "Maximum Tickets Per Customer" &&
+          `Maximum Tickets: ${purchasePolicy.maxTickets}`}
+        {purchasePolicy.type === "Lottery" &&
+          (<span>
+            Lottery Name: {purchasePolicy.lotteryName}{" "}
+            {isLotteryOpen(purchasePolicy.lotteryRegistrationDueDate)}
+          </span>)}
+      </span>
+    );
+  }
+
+  // COMPOSITE
+  return (
+    <span>
+      ({" "}
+
+      {purchasePolicy.leftPolicy && (
+        <ViewPurchasePolicies purchasePolicy={purchasePolicy.leftPolicy} />
+      )}
+
+      {" "}{purchasePolicy.operator}{" "}
+
+      {purchasePolicy.rightPolicy && (
+        <ViewPurchasePolicies purchasePolicy={purchasePolicy.rightPolicy} />
+      )}
+
+      {" "})
+    </span>
+  );
+}
+
+const isLotteryOpen = (dateStr: string) => {
+  const dueDate = new Date(dateStr);
+  const now = new Date();
+
+  return now <= dueDate ? "OPEN" : "CLOSED";
+};
