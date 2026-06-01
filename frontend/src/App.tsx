e7e40609cde37fa58e791a9b392f1294c7c3bfd2
NicoleGluhovsky <nicolegl@post.bgu.ac.il>
2026-06-01 12:27:56 +0300

    did the coloring wohoo pretty

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,6 +24,10 @@ import ViewEvent from "./Components/ViewEvent";
 import ViewOrder from "./Components/ViewOrder";
 
 import { useEffect, useState } from "react";
+import AssignMember from "./Components/AssignNewOwnerOrManager";
+import ChangeManagerPermissions, {
+  type ManagerPermissions,
+} from "./Components/ChangeManagerPermissions";
 import CreateOrderPage from "./Components/CreateOrder";
 import ViewAdminCompanyList from "./Components/ViewAdminCompanyList";
 import AdminPurchaseHistory from "./Components/ViewAdminPurcheseHistory";
@@ -31,11 +35,8 @@ import ProductionCompanyPurchaseHistory from "./Components/ViewProductionCompany
 import ViewUserCompanyList from "./Components/ViewUserCompanyList";
 import UserPurchaseHistory from "./Components/ViewUserPurchaseHistory";
 import ViewUsers from "./Components/ViewUsersList";
+import "./CSS/App.css";
 import { type VenueData } from "./DTOs/VenueDTO";
-import AssignMember from "./Components/AssignNewOwnerOrManager";
-import ChangeManagerPermissions,
-  {type ManagerPermissions  
-}from "./Components/ChangeManagerPermissions";
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
@@ -104,16 +105,27 @@ function App() {
   async function handleSubmitVenue(venueData: VenueData) {
     console.log("Venue saved:", venueData);
   }
-  async function handleAssignMember(data: Parameters<typeof AssignMember>[0]["onSubmit"] extends (d: infer D) => any ? D : never) {
+  async function handleAssignMember(
+    data: Parameters<typeof AssignMember>[0]["onSubmit"] extends (
+      d: infer D,
+    ) => any
+      ? D
+      : never,
+  ) {
     console.log("Member assigned:", data);
   }
-async function handleChangeManagerPermissions(
-  targetID: string,
-  companyID: number,
-  newPermissions: Set<ManagerPermissions>
-) {
-  console.log("Manager permissions changed:", targetID, companyID, newPermissions);
-}
+  async function handleChangeManagerPermissions(
+    targetID: string,
+    companyID: number,
+    newPermissions: Set<ManagerPermissions>,
+  ) {
+    console.log(
+      "Manager permissions changed:",
+      targetID,
+      companyID,
+      newPermissions,
+    );
+  }
 
   return (
     <BrowserRouter>
@@ -125,129 +137,141 @@ async function handleChangeManagerPermissions(
         >
           switch to {theme === "light" ? "dark" : "light"} mode
         </button>
+
+        <Routes>
+          <Route path="/events/:eventID" element={<ViewEvent />} />
+          <Route path="/orders/:orderID" element={<ViewOrder />} />
+          <Route
+            path="/checkout/"
+            element={
+              <PaymentForm amount={250} onPaymentSubmit={handlePayment} />
+            }
+          />
+
+          <Route
+            path="/create-purchase-policy/"
+            element={
+              <PurchasePolicyCreationForm
+                onCreatePolicy={handleCreatePurchasePolicy}
+              />
+            }
+          />
+
+          <Route
+            path="/create-production-company/"
+            element={
+              <CreateProductionCompany
+                onSubmit={handleCreateProductionCompany}
+              />
+            }
+          />
+
+          <Route
+            path="/company/:companyID/events"
+            element={
+              <ViewCompanyEvents
+                onEditEvent={handleEditEvent}
+                onDeleteEvent={handleDeleteEvent}
+              />
+            }
+          />
+
+          <Route
+            path="/create-event/"
+            element={<EventCreationForm onCreateEvent={handleCreateEvent} />}
+          />
+
+          <Route path="/events/search" element={<SearchEvents />} />
+
+          <Route
+            path="/company/:companyID/event-update/:eventID"
+            element={<EventUpdateForm onUpdateEvent={handleUpdateEvent} />}
+          />
+
+          <Route
+            path="/create-discount-policy/"
+            element={
+              <ViewDiscountPolicy onSubmit={handleDiscountPolicySubmit} />
+            }
+          />
+
+          <Route
+            path="/admin/view-users"
+            element={<ViewUsers users={undefined} />}
+          />
+
+          <Route
+            path="/admin/login"
+            element={
+              <LoginForm onLogin={handleAdminLogin} title="Admin Login" />
+            }
+          />
+
+          <Route
+            path="/user/login/member"
+            element={<LoginForm onLogin={handleUserLogin} title="User Login" />}
+          />
+          <Route
+            path="/admin/purchase-history"
+            element={<AdminPurchaseHistory />}
+          />
+          <Route
+            path="/user/purchase-history"
+            element={<UserPurchaseHistory />}
+          />
+          <Route
+            path="/company/:companyID/purchase-history"
+            element={<ProductionCompanyPurchaseHistory />}
+          />
+
+          <Route
+            path="/user/register"
+            element={
+              <RegistrationForm
+                onRegistration={handleUserRegistration}
+                title="User Registration"
+              />
+            }
+          />
+
+          <Route
+            path="/admin/register"
+            element={
+              <RegistrationForm
+                onRegistration={handleAdminRegistration}
+                title="Admin Registration"
+              />
+            }
+          />
+
+          <Route path="user/create-order" element={<CreateOrderPage />} />
+          <Route
+            path="/venue/:venueID?"
+            element={<VenueEditor onSubmitVenue={handleSubmitVenue} />}
+          />
+
+          <Route
+            path="user/:userID/companies"
+            element={
+              <ViewUserCompanyList onManageCompany={handleManageCompany} />
+            }
+          />
+          <Route path="admin/companies" element={<ViewAdminCompanyList />} />
+
+          <Route
+            path="/company/:companyID/assign-member"
+            element={<AssignMember onSubmit={handleAssignMember} />}
+          />
+          <Route
+            path="/company/:companyID/change-manager-permissions"
+            element={
+              <ChangeManagerPermissions
+                onSubmit={handleChangeManagerPermissions}
+              />
+            }
+          />
+        </Routes>
       </div>
-
-      <Routes>
-        <Route path="/events/:eventID" element={<ViewEvent />} />
-        <Route path="/orders/:orderID" element={<ViewOrder />} />
-        <Route
-          path="/checkout/"
-          element={<PaymentForm amount={250} onPaymentSubmit={handlePayment} />}
-        />
-
-        <Route
-          path="/create-purchase-policy/"
-          element={
-            <PurchasePolicyCreationForm
-              onCreatePolicy={handleCreatePurchasePolicy}
-            />
-          }
-        />
-
-        <Route
-          path="/create-production-company/"
-          element={
-            <CreateProductionCompany onSubmit={handleCreateProductionCompany} />
-          }
-        />
-
-        <Route
-          path="/company/:companyID/events"
-          element={
-            <ViewCompanyEvents
-              onEditEvent={handleEditEvent}
-              onDeleteEvent={handleDeleteEvent}
-            />
-          }
-        />
-
-        <Route
-          path="/create-event/"
-          element={<EventCreationForm onCreateEvent={handleCreateEvent} />}
-        />
-
-        <Route path="/events/search" element={<SearchEvents />} />
-
-        <Route
-          path="/company/:companyID/event-update/:eventID"
-          element={<EventUpdateForm onUpdateEvent={handleUpdateEvent} />}
-        />
-
-        <Route
-          path="/create-discount-policy/"
-          element={<ViewDiscountPolicy onSubmit={handleDiscountPolicySubmit} />}
-        />
-
-        <Route
-          path="/admin/view-users"
-          element={<ViewUsers users={undefined} />}
-        />
-
-        <Route
-          path="/admin/login"
-          element={<LoginForm onLogin={handleAdminLogin} title="Admin Login" />}
-        />
-
-        <Route
-          path="/user/login/member"
-          element={<LoginForm onLogin={handleUserLogin} title="User Login" />}
-        />
-        <Route
-          path="/admin/purchase-history"
-          element={<AdminPurchaseHistory />}
-        />
-        <Route
-          path="/user/purchase-history"
-          element={<UserPurchaseHistory />}
-        />
-        <Route
-          path="/company/:companyID/purchase-history"
-          element={<ProductionCompanyPurchaseHistory />}
-        />
-
-        <Route
-          path="/user/register"
-          element={
-            <RegistrationForm
-              onRegistration={handleUserRegistration}
-              title="User Registration"
-            />
-          }
-        />
-
-        <Route
-          path="/admin/register"
-          element={
-            <RegistrationForm
-              onRegistration={handleAdminRegistration}
-              title="Admin Registration"
-            />
-          }
-        />
-
-        <Route path="user/create-order" element={<CreateOrderPage />} />
-        <Route
-          path="/venue/:venueID?"
-          element={<VenueEditor onSubmitVenue={handleSubmitVenue} />}
-        />
-
-        <Route
-          path="user/:userID/companies"
-          element={
-            <ViewUserCompanyList onManageCompany={handleManageCompany} />
-          }
-        />
-        <Route path="admin/companies" element={<ViewAdminCompanyList />} />
-
-        <Route
-          path="/company/:companyID/assign-member"
-          element={<AssignMember onSubmit={handleAssignMember} />}
-        />
-        <Route
-          path="/company/:companyID/change-manager-permissions"
-          element={<ChangeManagerPermissions onSubmit={handleChangeManagerPermissions} />}
-        />
-      </Routes>
     </BrowserRouter>
   );
 }
