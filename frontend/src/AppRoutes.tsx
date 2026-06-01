14c90213321860d6bd1251ae91a86d4e308b56e7
NicoleGluhovsky <nicolegl@post.bgu.ac.il>
2026-06-01 15:40:35 +0300

    wow this is working we have some header !

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import SearchEvents from "./Components/SearchEvents";
+import LoginForm, { type LoginData } from "./Components/User/LoginForm";
+import type { RegistrationData } from "./Components/User/RegistrationForm";
+import RegistrationForm from "./Components/User/RegistrationForm";
+
+export default function AppRoutes() {
+  async function handleLogin(data: LoginData) {
+    console.log("Logging in with data:", data);
+    return Promise.resolve();
+  }
+
+  async function handleRegister(data: RegistrationData) {
+    console.log("Registering with data:", data);
+    return Promise.resolve();
+  }
+
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={<LoginForm title="Login" onLogin={handleLogin} />}
+      />
+      <Route
+        path="/register"
+        element={
+          <RegistrationForm title="Register" onRegistration={handleRegister} />
+        }
+      />
+
+      <Route path="/events/search" element={<SearchEvents />} />
+    </Routes>
+  );
+}
