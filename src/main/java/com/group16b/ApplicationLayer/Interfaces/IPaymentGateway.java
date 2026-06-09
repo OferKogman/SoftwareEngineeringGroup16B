cbd7305cdfd5c4751436f302a74f060a4c2b20bc
david podvinsky <davidpod@post.bgu.ac.il>
2026-06-09 16:18:10 +0300

    implemented poccessPaymen, a.k.a pay, canged the signature to return transaction id, since refund wants the transaction id, subsequentially, i will have to tamper a little with finalize order to work correctly

--- src/main/java/com/group16b/ApplicationLayer/Interfaces/IPaymentGateway.java
+++ src/main/java/com/group16b/ApplicationLayer/Interfaces/IPaymentGateway.java
@@ -3,7 +3,7 @@ package com.group16b.ApplicationLayer.Interfaces;
 import com.group16b.ApplicationLayer.Records.PaymentInfo;
 
 public interface IPaymentGateway {
-    void processPayment(PaymentInfo paymentInfo, double price);
+    int processPayment(PaymentInfo paymentInfo, double price);
     void cancelPayment();
 
     
--- src/main/java/com/group16b/InfrastructureLayer/PaymentService.java
+++ src/main/java/com/group16b/InfrastructureLayer/PaymentService.java
@@ -3,20 +3,52 @@ package com.group16b.InfrastructureLayer;
 import com.group16b.ApplicationLayer.Exceptions.PaymentFailedException;
 import com.group16b.ApplicationLayer.Interfaces.IPaymentGateway;
 import com.group16b.ApplicationLayer.Records.PaymentInfo;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService implements IPaymentGateway {
-    public void processPayment(PaymentInfo paymentInfo, double price){
-        if (paymentInfo == null || price <= 0) {
-            throw new PaymentFailedException("Payment processing failed for payment info: " + paymentInfo + " with price: " + price);
-        }
+
+    private static final String BASE_URL="https://damp-lynna-wsep-1984852e.koyeb.app/";
+
+    private final RestTemplate restTemplate;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    @Override
-    public void cancelPayment() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cancelPayment'");
+    //pay, returns the transaction id if successful, or -1 on fail
+    public int processPayment(PaymentInfo paymentInfo, double price)
+    {
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("action_type","pay");
+        requestBody.add("currency", paymentInfo.currency());
+        requestBody.add("cardNumber", paymentInfo.cardNumber());
+        requestBody.add("month", String.valueOf(paymentInfo.month()));
+        requestBody.add("year", String.valueOf(paymentInfo.year()));
+        requestBody.add("holder", paymentInfo.holder());
+        requestBody.add("cvv", paymentInfo.cvv());
+        requestBody.add("id", paymentInfo.id());
+        requestBody.add("price", String.valueOf(price));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(BASE_URL, requestEntity, String.class);
+
+        return Integer.parseInt(response.getBody().trim());
     }
+
+    public void cancelPayment(){}
 
 }
