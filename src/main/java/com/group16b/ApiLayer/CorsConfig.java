351f4de4b01dedbe3b7694a5769a97d2b2d5a6b2
unknown <idofraiberg420@gmail.com>
2026-06-15 05:41:01 +0300

    removed stupid cors changes, weren't required

--- src/main/java/com/group16b/ApiLayer/CorsConfig.java
+++ src/main/java/com/group16b/ApiLayer/CorsConfig.java
@@ -10,9 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*")
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
