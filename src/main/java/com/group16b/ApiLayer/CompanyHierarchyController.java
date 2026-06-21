666b705ba1bfc22d293a29f85ea2569bc37d986e
david podvinsky <davidpod@post.bgu.ac.il>
2026-06-21 14:01:08 +0300

    removed the /api in the urls, will have to check if i also need to revert to controller in LP controller or if i can keep the RestController there

--- src/main/java/com/group16b/ApiLayer/CompanyHierarchyController.java
+++ src/main/java/com/group16b/ApiLayer/CompanyHierarchyController.java
@@ -18,7 +18,7 @@ import com.group16b.ApplicationLayer.DTOs.ChangeManagerPermissionRequestDTO;
 import com.group16b.ApplicationLayer.DTOs.InviteHandleRequestDTO;
 
 @RestController
-@RequestMapping("/api/production-companies/{companyId}")
+@RequestMapping("/production-companies/{companyId}")
 public class CompanyHierarchyController extends BaseController {
     private final CompanyHierarchyService companyHierarchyService;
 
