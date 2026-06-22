34d5c5a4cad5847f84eee393625df070e3c349fb
unknown <idofraiberg420@gmail.com>
2026-06-23 02:31:22 +0300

    PLEASE

--- src/main/java/com/group16b/ApplicationLayer/ReserveService.java
+++ src/main/java/com/group16b/ApplicationLayer/ReserveService.java
@@ -1,9 +1,11 @@
 package com.group16b.ApplicationLayer;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.group16b.DomainLayer.Policies.DiscountPolicy.DiscountContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.OptimisticLockingFailureException;
@@ -340,9 +342,8 @@ public class ReserveService {
         }
 
         double priceAfterDiscountPolicy = pricePerSeat * amount;
-
         for (DiscountPolicy dp : allPolicies) {
-            priceAfterDiscountPolicy = dp.calculateDiscount(priceAfterDiscountPolicy);
+            priceAfterDiscountPolicy = dp.calculateDiscount(priceAfterDiscountPolicy, new DiscountContext(10, amount, LocalDateTime.now(), null));
         }
         return priceAfterDiscountPolicy;
     }
