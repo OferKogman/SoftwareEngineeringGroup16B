40b3484dfd60c086654b2a4e90015ed20bbdd229
david podvinsky <davidpod@post.bgu.ac.il>
2026-06-28 15:28:03 +0300

    no idea what im doing, added the repository, and it looks like im supposed to remove the final from all final fields, and maybe recheck anotations?

--- src/main/java/com/group16b/DomainLayer/Venue/VenueGrid.java
+++ src/main/java/com/group16b/DomainLayer/Venue/VenueGrid.java
@@ -1,5 +1,7 @@
 package com.group16b.DomainLayer.Venue;
 
+import java.util.Objects;
+
 import jakarta.persistence.Embeddable;
 
 @Embeddable
@@ -16,4 +18,22 @@ public class VenueGrid {
 
     public int getRows() { return rows; }
     public int getColumns() { return columns; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof VenueGrid)) {
+            return false;
+        }
+        VenueGrid other = (VenueGrid) o;
+        return rows == other.rows &&
+            columns == other.columns;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rows, columns);
+    }
 }
