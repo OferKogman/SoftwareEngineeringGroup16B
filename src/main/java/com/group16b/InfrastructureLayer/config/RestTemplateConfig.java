3068b5aa85891032403100daea96848c72b48090
unknown <idofraiberg420@gmail.com>
2026-07-01 22:00:19 +0300

    Load WSEP timeout configuration

--- src/main/java/com/group16b/InfrastructureLayer/config/RestTemplateConfig.java
+++ src/main/java/com/group16b/InfrastructureLayer/config/RestTemplateConfig.java
@@ -5,15 +5,17 @@ import java.time.Duration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
 
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder, @Value("${wsep.connect-timeout-seconds}") long connectTimeoutSeconds, @Value("${wsep.read-timeout-seconds}") long readTimeoutSeconds) {
+    public RestTemplate restTemplate(RestTemplateBuilder builder, @Value("${wsep.connect-timeout-seconds}") long connectTimeoutSeconds,
+                                     @Value("${wsep.read-timeout-seconds}") long readTimeoutSeconds) {
         return builder.setConnectTimeout(Duration.ofSeconds(connectTimeoutSeconds))
                 .setReadTimeout(Duration.ofSeconds(readTimeoutSeconds))
                 .build();
