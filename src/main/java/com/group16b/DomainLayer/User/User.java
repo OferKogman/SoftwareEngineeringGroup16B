88da3266c9dcf4d58982aef2b545cfe1769e8aec
NicoleGluhovsky <nicolegl@post.bgu.ac.il>
2026-07-01 12:56:54 +0300

    venuevenuevenu

--- src/main/java/com/group16b/DomainLayer/User/User.java
+++ src/main/java/com/group16b/DomainLayer/User/User.java
@@ -1,6 +1,5 @@
 package com.group16b.DomainLayer.User;
 
-
 import java.security.MessageDigest;
 
 import jakarta.persistence.Column;
@@ -8,12 +7,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
+
 @Entity
 @Table(name = "users")
 public class User {
 
 	@Id
-    @Column(nullable = false, unique = true)
+	@Column(nullable = false, unique = true)
 	private String email;
 
 	@Column(nullable = false)
@@ -22,7 +22,8 @@ public class User {
 	@Version
 	private long version;
 
-	protected User() {}//required for jpa
+	protected User() {
+	}// required for jpa
 
 	public User(String email, String password) {
 		this.email = email;
@@ -30,12 +31,12 @@ public class User {
 	}
 
 	public User(User user) {
-		this.email = user.getEmail();//changed for mocked testing userRepo
+		this.email = user.getEmail();// changed for mocked testing userRepo
 		this.password = user.password;
 		this.version = user.getVersion();
-    }
+	}
 
-    public String getEmail() {
+	public String getEmail() {
 		return email;
 	}
 
@@ -49,19 +50,19 @@ public class User {
 			System.out.println("Error hashing password: " + e.getMessage());
 		}
 	}
-	
+
 	public void changePassword(String oldPassword, String newPassword) {
-		
+
 		if (!confirmPassword(oldPassword)) {
 			throw new IllegalArgumentException("Old password is incorrect.");
 		}
 
 		if (confirmPassword(newPassword)) {
-            throw new IllegalArgumentException("New password cannot be the same as the old password.");
-        }
-		
+			throw new IllegalArgumentException("New password cannot be the same as the old password.");
+		}
+
 		setPassword(newPassword);
-		
+
 	}
 
 	public boolean confirmPassword(String password) {
@@ -72,26 +73,26 @@ public class User {
 			return this.password.equals(stringHash);
 		} catch (Exception e) {
 			System.out.println("Error hashing password: " + e.getMessage());
-			return false;	
+			return false;
 		}
 	}
 
-    public long getVersion() {
+	public long getVersion() {
 		return version;
-    }
+	}
 
-		public void incrementVersion() {
+	public void incrementVersion() {
 		this.version++;
 	}
 
-    public void update(User newUser) {
+	public void update(User newUser) {
 		this.email = newUser.email;
 		this.password = newUser.password;
-		
-    }
 
-    public void setVersion(long newVersion) {
+	}
+
+	public void setVersion(long newVersion) {
 		this.version = newVersion;
-    }
+	}
 
 }
